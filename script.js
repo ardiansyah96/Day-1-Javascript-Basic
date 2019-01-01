8c7ec26e677a67e3b332ded57282491e1d4ba8a4
ardiansyah96 <ardiansyah142002@gmail.com>
2019-01-01 22:11:38 +0800

    lagi belajar

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// variabel corcounce
+var firstName = 'john';
+var age = 28;
+
+var job, isMarried;
+job = 'teacher';
+isMarried = false;
+
+console.log(firstName + ' ' + age + ' ' + job + ' ' + isMarried);
+// variabel mutation
+firstName = 'ardi';
+age = 'twenty eight';
+job = 'programmer';
+alert(firstName + ' ' + age + ' ' + job + ' ' + isMarried);
+var lastName = prompt(" how is last name ? ");
+alert(firstName + ' ' + lastName);
